fea96163c65197eb50fb1a11010499e6dc19c298
Gabriela Aguiar <aguiar.gabi95@gmail.com>
2020-08-07 19:21:43 -0300

    class 3 nunjuck

--- src/server.js
+++ src/server.js
@@ -21,17 +21,29 @@ const proffys = [
     }
 ]
 
+const pageLanding = (req, res) =>{
+    return res.render('index.html')
+}
+
+const pageStudy = (req, res) =>{
+    return res.render('study.html', {proffys})
+}
+
+const pageGiveClasses = (req, res) =>{
+    return res.render('give-classes.html')
+}
+
 const express = require('express')
 const server = express()
+const nunjucks = require('nunjucks')
+
+nunjucks.configure('src/views', {
+    express:server,
+    noCache:true,
+})
 
 server.use(express.static('public'))
-.get('/', (req, res) => {
-    return res.sendFile(__dirname + '/views/index.html')
-})
-.get('/study', (req, res) => {
-    return res.sendFile(__dirname + '/views/study.html')
-})
-.get('/give-classes', (req, res) => {
-    return res.sendFile(__dirname + '/views/give-classes.html')
-})
+.get('/', pageLanding)
+.get('/study', pageStudy)
+.get('/give-classes', pageGiveClasses)
 .listen(5500)
